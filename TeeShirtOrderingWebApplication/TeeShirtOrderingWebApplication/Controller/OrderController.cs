c1ef398be6c7808e618593b65143e207dc4b5396
djkolesa <djkolesa@gmail.com>
2021-02-09 15:45:04 -0800

    David Kolesar is working on submitting order through front end

---
+++ TeeShirtOrderingWebApplication/TeeShirtOrderingWebApplication/Controller/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderProcessingApplication.Controller
+{
+    public class OrderController
+    {
+
+	public String takeInput(String input)
+		{
+
+			//sanitize data
+
+			//return for now
+			return "Hello front-end!";
+		}
+
+	}
+}
